1a632e2614dc1cd097d82dd1413febcfbc74b271
Mercy Akindele <mercyakinlawon@gmail.com>
2022-05-27 21:07:52 -0500

    second commit added props to components

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
-
+  
+  function deleteRecipe(targetIndex){
+    const newRecipeArray= recipes.filter((recipe, index) =>{
+      return index !== targetIndex;
+    })
+    setRecipes(newRecipeArray);
+  }
+     const addToList= (additionalRecipe) => setRecipes([ ...recipes, additionalRecipe]);
+    
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
 
   
   return (
     <div className="App">
-      <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <header><h1 className="header">Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
+      <RecipeCreate addToList={addToList}/>
+      
     </div>
   );
 }
